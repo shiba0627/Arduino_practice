efdcd67001af539f0ad57d58c6128f9854be72e0
shiba0627 <1E8EC3@gmail.com>
2025-04-24 07:30:33 +0900

    LEDを点滅

---
+++ LED/LED.ino
@@ -0,0 +1,14 @@
+//LEDの点滅
+int led1 = 6;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led1, HIGH);
+  delay(500);
+  disitalWrite(led1, LOW);
+  delay(500);
+}
