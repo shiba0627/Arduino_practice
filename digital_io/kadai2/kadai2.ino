1d02e26a219af4eda9158ea994cb9d6f2835bf28
shiba0627 <1E8EC3@gmail.com>
2025-05-07 22:11:45 +0900

    課題2を追記

--- digital_io/kadai2/kadai2.ino
+++ digital_io/kadai2/kadai2.ino
@@ -2,6 +2,10 @@ int ledPin = 5;
 int buttonApin = 8;
 int buttonBpin = 9;
 
+const int SW_pin = 2; // digital pin connected to switch output
+const int X_pin = A0; // analog pin connected to X output
+const int Y_pin = A1; // analog pin connected to Y output
+
 byte leds = 0;
 
 void setup() 
@@ -14,26 +18,31 @@ void setup()
 
 void loop() 
 {
+  Serial.print("(");
+  Serial.print(analogRead(X_pin));
+  Serial.print(",");
+  Serial.print(analogRead(Y_pin));
+  Serial.print(",");
   if (digitalRead(buttonApin) == LOW && digitalRead(buttonBpin) == LOW)//どちらも押されている
   {
-    Serial.print("(1,1)");
+    
+    Serial.print("1,1)\n");
     digitalWrite(ledPin, HIGH);
   }
   if (digitalRead(buttonApin) == HIGH && digitalRead(buttonBpin) == HIGH)//どちらも押されていない
   {
-    Serial.print("(0,0)");
+    Serial.print("0,0)\n");
     digitalWrite(ledPin, LOW);
   }
   if (digitalRead(buttonApin) == LOW && digitalRead(buttonBpin) == HIGH)//Aが押されていて、Bが押されていない
   {
-    Serial.print("(1,0)");
+    Serial.print("1,0)\n");
     digitalWrite(ledPin, HIGH);
   }
   if (digitalRead(buttonApin) == HIGH && digitalRead(buttonBpin) == LOW)//Aが押されていなくて、Bが押されている
   {
-    Serial.print("(0,1)");
+    Serial.print("0,1)\n");
     digitalWrite(ledPin, LOW);
   }
-  Serial.print("\n");
   delay(100);
 }
